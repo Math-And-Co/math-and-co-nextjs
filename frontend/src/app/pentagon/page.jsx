7d91d78b8b756bea6524e7e2f9a202261cae56e0
Koketso M Dibakwana <103131298+Spax19@users.noreply.github.com>
2025-04-14 13:32:56 +0200

    ...

--- frontend/src/app/pentagon/page.jsx
+++ frontend/src/app/pentagon/page.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import Footer from '../../components/footer';
-import Navbar from '../../components/Navbar';
+import Navbar from '../../components/navbar';
 
 const PentagonPage = () => {
     const [activeCollection, setActiveCollection] = useState('reserve');
--- frontend/src/app/shop/page.jsx
+++ frontend/src/app/shop/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import Navbar from "../../components/Navbar";
+import Navbar from "../../components/navbar";
 import Footer from "../../components/footer";
 
 function ShopPage() {
