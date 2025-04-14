031eda6c899b4b15415c8a10f0d39a529a1d0a83
Koketso M Dibakwana <103131298+Spax19@users.noreply.github.com>
2025-04-14 13:44:35 +0200

    ...

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Image from 'next/image';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/navbar';
 import Footer from "../components/footer";
 import Link from 'next/link';
 
